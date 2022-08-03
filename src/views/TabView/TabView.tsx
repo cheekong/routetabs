246791f3cc8f0dd840c28f5a37b1538c5dabea3b
Chee <cheekong.t@gmail.com>
2022-08-03 10:32:50 +1000

    chore(): remove unused var

--- src/views/TabView/TabView.tsx
+++ src/views/TabView/TabView.tsx
@@ -4,7 +4,6 @@ import { Tabs, TabList, Tab, TabPanel } from "../../components/";
 import "./TabView.css";
 
 export const TabView: FC = () => {
-  const navigate = useNavigate();
   const pathName = useLocation().pathname;
 
   return (
